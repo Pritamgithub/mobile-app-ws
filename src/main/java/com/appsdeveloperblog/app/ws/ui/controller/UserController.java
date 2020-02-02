dc2e9efbbe4a1722bbd2bd92059640cb412b4597
Pritam Chavhan <pritam0263@gmail.com>
2020-02-01 19:31:37 -0500

    Initial commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.app.ws.ui.controller.transport.UserDataTransferObject;
+import com.appsdeveloperblog.app.ws.ui.controller.transport.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.ui.controller.transport.UserRest;
+import com.appsdeveloperblog.app.ws.ui.service.UserService;
+
+@RestController // willl be able to receive http request
+@RequestMapping("users") // url tht is responsible for creating the path
+
+//http://localhost:8080/users
+public class UserController {
+	@Autowired
+	UserService userService;
+
+	@GetMapping
+	public String getUser() {
+		return "get user was called";
+	}
+
+	@PostMapping
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetail) { // userdetails request input from the
+																					// user
+
+		UserRest returnValue = new UserRest(); // response
+		UserDataTransferObject userDto = new UserDataTransferObject(); // userdatatransfer object is class that can be
+																		// shared across all the layers
+		BeanUtils.copyProperties(userDetail, userDto);
+		UserDataTransferObject createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+
+		return returnValue;
+	}
+
+	@PutMapping
+	public String updateUser() {
+		return "update user was called";
+	}
+
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user was called";
+
+	}
+}
---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/service/UserService.java
@@ -0,0 +1,12 @@
+package com.appsdeveloperblog.app.ws.ui.service;
+
+import com.appsdeveloperblog.app.ws.ui.controller.transport.UserDataTransferObject;
+
+public class UserService {
+
+	public UserDataTransferObject createUser(UserDataTransferObject userDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
